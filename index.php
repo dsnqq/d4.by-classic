bc45b62e76c024cbd611beabba9713958cbe7be5
Korzhich.d <korzhich.d@promail366.com>
2026-03-23 14:11:34 +0300

    v2pagecache disenable

--- index.php
+++ index.php
@@ -10,14 +10,6 @@ if (is_file('config.php')) {
 	require_once('config.php');
 }
 
-// v2pagecache: try to serve from page cache early
-require_once(DIR_SYSTEM . 'library/v2pagecache.php');
-$pagecache = new V2PageCache();
-if ($pagecache->ServeFromCache()) {
-	// If we served this page from the cache, stop processing
-	return;
-}
-
 // Install
 if (!defined('DIR_APPLICATION')) {
 	header('Location: install/index.php');
@@ -28,8 +20,3 @@ if (!defined('DIR_APPLICATION')) {
 require_once(DIR_SYSTEM . 'startup.php');
 
 start('catalog');
-
-// v2pagecache: store generated html into cache
-if ($pagecache->OkToCache()) {
-	$pagecache->CachePage();
-}
