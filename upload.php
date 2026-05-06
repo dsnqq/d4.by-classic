b56ac68039172475a743802c5441822f926a82f3
Korzhich.d <korzhich.d@promail366.com>
2026-05-06 15:18:28 +0300

    Revert "upload fixed photo to 90grad"
    This reverts commit 85afc896ca7145dea1e7f1affcaef3ca7317d427.

--- upload.php
+++ upload.php
@@ -8,58 +8,6 @@ set_time_limit(0);
 
 header('Content-Type: text/plain; charset=utf-8');
 
-function normalizeJpegOrientation($file_path) {
-	if (!function_exists('exif_read_data') || !function_exists('imagecreatefromjpeg') || !function_exists('imagejpeg') || !function_exists('imagerotate')) {
-		return;
-	}
-
-	$image_info = @getimagesize($file_path);
-	if (!$image_info || !isset($image_info[2]) || (int)$image_info[2] !== IMAGETYPE_JPEG) {
-		return;
-	}
-
-	$exif = @exif_read_data($file_path);
-	$orientation = isset($exif['Orientation']) ? (int)$exif['Orientation'] : 1;
-
-	if ($orientation === 1) {
-		return;
-	}
-
-	$image = @imagecreatefromjpeg($file_path);
-	if (!$image) {
-		return;
-	}
-
-	switch ($orientation) {
-		case 3:
-			$rotated = imagerotate($image, 180, 0);
-			imagedestroy($image);
-			$image = $rotated;
-			break;
-		case 6:
-			$rotated = imagerotate($image, -90, 0);
-			imagedestroy($image);
-			$image = $rotated;
-			break;
-		case 8:
-			$rotated = imagerotate($image, 90, 0);
-			imagedestroy($image);
-			$image = $rotated;
-			break;
-		default:
-			// Unsupported EXIF orientation variants (mirrored).
-			imagedestroy($image);
-			return;
-	}
-
-	if (!$image) {
-		return;
-	}
-
-	imagejpeg($image, $file_path, 92);
-	imagedestroy($image);
-}
-
 $base = __DIR__;
 $date_now = date('d.m.Y');
 $time = (int) strtotime($date_now);
@@ -99,8 +47,6 @@ if (!move_uploaded_file($_FILES['file']['tmp_name'], $dest)) {
 	echo 'move_failed';
 	exit;
 }
-
-normalizeJpegOrientation($dest);
 
 @chmod($dest, 0664);
 http_response_code(200);
